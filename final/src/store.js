ca9a3f13f5d786ea19ca6670ceb488e51dd802fc
smajaas <smajaas@gmail.com>
2022-11-28 00:30:25 +0530

    display featured product completed

--- final/src/store.js
+++ final/src/store.js
@@ -1,5 +1,5 @@
 import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
+let store = getStorageItem('store');
 const setupStore = (products) => {
   store = products.map((product) => {
     const {
@@ -9,7 +9,9 @@ const setupStore = (products) => {
     const image = img[0].thumbnails.large.url;
     return { id, featured, name, price, company, colors, image };
   });
+  setStorageItem('store', store);
 };
+console.log(store);
 
 const findProduct = () => {};
 export { store, setupStore, findProduct };
--- final/src/utils.js
+++ final/src/utils.js
@@ -17,10 +17,26 @@ const getElement = (selection) => {
   );
 };
 
-const formatPrice = () => {};
+const formatPrice = (price) => {
+  let formattedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format((price / 100).toFixed(2));
+  return formattedPrice;
+};
 
-const getStorageItem = () => {};
-const setStorageItem = () => {};
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  } else {
+    storageItem = [];
+  }
+  return storageItem;
+};
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
 
 export {
   allProductsUrl,
