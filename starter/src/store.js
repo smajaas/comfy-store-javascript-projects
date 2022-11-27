29a419e52773b4ba4f508c93607b67da3be8b089
smajaas <smajaas@gmail.com>
2022-11-27 22:58:01 +0530

    setup store and cart

---
+++ starter/src/store.js
@@ -0,0 +1,15 @@
+import { getStorageItem, setStorageItem } from './utils.js';
+let store = [];
+const setupStore = (products) => {
+  store = products.map((product) => {
+    const {
+      id,
+      fields: { featured, name, price, company, colors, image: img },
+    } = product;
+    const image = img[0].thumbnails.large.url;
+    return { id, featured, name, price, company, colors, image };
+  });
+};
+
+const findProduct = () => {};
+export { store, setupStore, findProduct };
